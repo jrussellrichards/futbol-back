16b50bf689c3cade23c71d99793a4caba253c929
javman92 <richards.javier@gmail.com>
2020-03-28 20:49:28 -0300

    se agrega procFile

--- config.js
+++ config.js
@@ -7,11 +7,3 @@ module.exports = new Sequelize('museonat_futbol', 'museonat_javier', 'javier123.
 });
 
 
-
-// module.exports  = mysql.createConnection({
-//   host: 'ftp.museonaturalia.cl',
-//   user: 'museonat_javier',
-//   password: 'javier123.',
-//   database: 'museonat_shells'
-    
-// });
