8c1f2f160298ae03029e06751155e6517bedb921
javman92 <richards.javier@gmail.com>
2020-03-29 00:27:35 -0300

    se mejora posicion de llamada cors en server.js

--- server.js
+++ server.js
@@ -5,32 +5,15 @@ const port = process.env.PORT || 5000;
 const Routes = require('./Routes/tournamentRequestRoutes');
 app.use(express.json());
 
-
-Routes(app)
-
-app.use(express.json());
-
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// parse requests of content-type - application/json
-
-// MONGO DB CONNECTION
-// mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
-// const connection = mongoose.connection;
-
-// connection.once("open", function() {
-//   console.log("MongoDB database connection established successfully");
-// });
-
-// Base Route
+Routes(app)
 
 
-// Campeonato Route
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
 
